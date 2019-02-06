5d3c109e1996ed0b4906831dabbde6b5736390b2
Hogoworld <hogoworld@hogoworlds-mac-mini.local>
2019-02-06 12:40:11 +0530

    adding back function in second view controller Added button in storyboard in second view

--- bob_basics/secondViewController.swift
+++ bob_basics/secondViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class secondViewController: UIViewController {
 
+    @IBAction func backAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
