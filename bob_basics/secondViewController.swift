a02d4e23238389c80b1f301df2261d44ac3d334b
Hogoworld <hogoworld@hogoworlds-mac-mini.local>
2019-02-06 15:22:59 +0530

    1.secondview added 2.added file

---
+++ bob_basics/secondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  secondViewController.swift
+//  bob_basics
+//
+//  Created by Hogoworld on 06/02/19.
+//  Copyright © 2019 Hogoworld. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
